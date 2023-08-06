bc897e178959c1c6469b532efb56534d247326a2
codewarsfx <chiderainnocent001@gmail.com>
2023-08-06 11:24:26 +0100

    initialize go app'

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conference = "hello"
+	const  conferenceTickets = 50
+	var remainingTickets = 50
+
+
+	
+	fmt.Println("welcome to our",conference,"booking application")
+	fmt.Println("we have a total of",conferenceTickets,"tickets and ", remainingTickets, "are still remaining")
+	fmt.Println("Get your tickets to attend ")
+
+	
+
+
+}
